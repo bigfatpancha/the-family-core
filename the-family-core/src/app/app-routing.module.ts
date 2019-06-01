b376455f957056688aabff4cba7f6feb8f496300
Lucas <lucas.scazza@gmail.com>
2019-06-01 14:43:42 -0300

    develop/ agrego las rutas

--- the-family-core/src/app/app-routing.module.ts
+++ the-family-core/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from '../app/components/dashboard/dashboard.component';
+import { UsersComponent } from '../app/components/users/users.component';
+import { NewUserComponent } from '../app/components/new-user/new-user.component';
+import { UploadComponent } from '../app/components/upload/upload.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'newuser', component: NewUserComponent },
+  { path: 'upload', component: UploadComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

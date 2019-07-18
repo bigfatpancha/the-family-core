5ef9a96de1309645090171677e12a4892bc29fca
Lucia <lucia.julia.r@gmail.com>
2019-07-17 23:29:19 -0300

    rewards funcionando

--- the-family-core/src/app/components/rewards/rewards.component.ts
+++ the-family-core/src/app/components/rewards/rewards.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/services/users/users.service';
-import { FamilyUser } from 'src/app/model/family';
+import { FamilyUser, FamilyUserListResponse } from 'src/app/model/family';
+import { User } from 'src/app/model/auth';
 
 @Component({
   selector: 'app-rewards',
@@ -16,6 +17,30 @@ export class RewardsComponent implements OnInit {
 
   ngOnInit() {
     this.users = this.usersService.users;
+    this.users.sort((user1, user2) => {
+      if (user1.stars < user2.stars) {
+        return 1;
+      } else if (user1.stars > user2.stars) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+  redeem(userGeneric: FamilyUser) {
+    let user: User = new User();
+    user.stars = 0;
+    user.username = userGeneric.username;
+    user.nickname = userGeneric.nickname;
+    user.email = userGeneric.email;
+    this.usersService.doUserIdPut(userGeneric.id, user).subscribe((res: User) => {
+      alert('Redeem Successful');
+      this.usersService.doGetUsersList().subscribe((res: FamilyUserListResponse) => {
+        this.users = res.results;
+        this.usersService.users = res.results;
+      })
+    })
   }
 
 }

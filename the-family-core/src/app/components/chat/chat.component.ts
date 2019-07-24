d64af0d3302a861aa94f77239efc97ed22daa3a6
Lucia <lucia.julia.r@gmail.com>
2019-07-24 02:44:22 -0300

    integracion sendbird

--- the-family-core/src/app/components/chat/chat.component.ts
+++ the-family-core/src/app/components/chat/chat.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from 'src/app/model/auth';
 import { SendbirdService } from 'src/app/services/sendbird/sendbird.service';
+import { UsersService } from 'src/app/services/users/users.service';
+import { FamilyUser } from 'src/app/model/family';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
@@ -13,11 +16,96 @@ export class ChatComponent implements OnInit {
 
   chatListOpen = false;
   converOpen = false;
+  channelList: any;
+  users: FamilyUser[];
+  selectedUsers: FamilyUser[];
+  showSelect= false;
+  dropdownSettings = {
+    singleSelection: false,
+    idField: 'sendbirdId',
+    textField: 'nickname',
+    placeholder: 'Select user'
+  };
+  chat;
+  messages;
 
-  constructor(private sbService: SendbirdService) { }
+  formList: FormGroup;
+  formChat: FormGroup;
+
+  constructor(
+    private sbService: SendbirdService,
+    private usersService: UsersService
+  ) { }
 
   ngOnInit() {
-    this.sbService.connect(this.user.sendbirdId);
+    this.users = this.usersService.users.filter((user: FamilyUser) => {
+      return user.id !== this.user.id;
+    });
+    this.formList = new FormGroup({
+      'chats': new FormControl([])
+    })
+    this.formChat = new FormGroup({
+      'message': new FormControl(null)
+    })
+    this.sbService.connect(this.user.sendbirdId).then((user: any) => {
+      this.sbService.getChannelList().then((channelList: any) => {
+        this.channelList = channelList;
+        console.log(this.channelList);
+      });
+    });
+    
+  }
+
+  get chats() { return this.formList.get('chats'); }
+  get message() { return this.formChat.get('message'); }
+
+  showList() {
+    this.showSelect = !this.showSelect;
+  }
+
+  newChat() {
+    this.showSelect = false;
+    const ids = this.chats.value.map((user: FamilyUser) => user.sendbirdId);
+    const names = this.chats.value.map((user: FamilyUser) => user.nickname);
+    let name = '';
+    names.forEach(nickname => {
+      name += nickname + ' ';
+    });
+    this.sbService.startGroupChannel(name, ids).then((groupChannel: any) => {
+      this.sbService.getChannelList().then((channelList) => {
+        this.channelList = channelList;
+        console.log(this.channelList);
+      })
+    });
+  }
+
+  openChat(chat) {
+    this.converOpen = true;
+    this.chat = chat;
+    this.sbService.loadPreviousMessages(chat).then((messages) => {
+      this.messages = messages.slice().reverse();
+    })
+  }
+
+  getName() {
+    return this.getNameChat(this.chat);
+  }
+
+  getNameChat(chat) {
+    let name = '';
+    chat.members.forEach(member => {
+      name += member.nickname + ' ';
+    });
+    return name;
+  }
+
+  sendmessage() {
+    this.sbService.sendMessage(this.message.value, this.chat).then((message) => {
+      this.sbService.loadPreviousMessages(this.chat).then((messages) => {
+        this.messages = messages.slice().reverse();
+        console.log(this.messages);
+      })
+    })
   }
 
   closeChat(){
--- the-family-core/src/app/services/sendbird/sendbird.service.ts
+++ the-family-core/src/app/services/sendbird/sendbird.service.ts
@@ -9,31 +9,50 @@ export class SendbirdService implements OnDestroy {
   sendbird;
   id: string;
   user;
+  groupChannel: any;
+  HANDLER = 'handler';
 
   constructor() {
     this.sendbird = new SendBird({appId: '24FA9059-6DA2-49AF-932A-8790F065C3E0'});
-  }
+    var ChannelHandler = new this.sendbird.ChannelHandler();
 
-  connect(id: string) {
+    ChannelHandler.onMessageReceived = function(channel, message) {};
+    ChannelHandler.onUserReceivedInvitation = function(groupChannel, inviter, invitees) {};
+    ChannelHandler.onUserDeclinedInvitation = function(groupChannel, inviter, invitee) {};
+
+    // Add this channel event handler to the SendBird object.
+    this.sendbird.addChannelHandler(this.HANDLER, ChannelHandler);
+  }  
+
+  connect(id: string): Promise<any> {
     this.id = id;
-    this.sendbird.connect(id, function(user, error) {
-      this.user = user;
-      if (error) {
-          return;
-      }
-  });
-  }
-
-  startGroupChannel(id: string) {
-    var userIds = [this.id, id];
-
-    this.sendbird.GroupChannel.createChannelWithUserIds(userIds, true, function(groupChannel, error) {
+    const promise = new Promise((resolve, reject) => {
+      this.sendbird.connect(id, function(user, error) {
+      
         if (error) {
-            return;
+            reject(error);
+        } else {
+          console.log('conectado a sendbird', user);
+          resolve(user);
         }
         
-        console.log(groupChannel);
+      });
     });
+    return promise;
+  }
+
+  startGroupChannel(name: string, ids: string[]): Promise<any> {
+    ids.push(this.id);
+    console.log(name, ids)
+    const promise = new Promise((resolve, reject) => {
+      this.sendbird.GroupChannel.createChannelWithUserIds(ids, true, name, function(groupChannel, error) {
+        if (error) {
+            reject(error);
+        }
+        resolve(groupChannel);
+      });
+    });
+    return promise;
   }
 
   leaveChannel(groupChannel) {
@@ -54,21 +73,76 @@ export class SendbirdService implements OnDestroy {
     });
   }
 
-  getChannelList() {
+  getChannelList(): Promise<any> {
     var channelListQuery = this.sendbird.GroupChannel.createMyGroupChannelListQuery();
     channelListQuery.includeEmpty = true;
     channelListQuery.limit = 20;    // The value of pagination limit could be set up to 100.
-
-    if (channelListQuery.hasNext) {
+    const promise = new Promise((resolve, reject) => {
+      if (channelListQuery.hasNext) {
         channelListQuery.next(function(channelList, error) {
             if (error) {
-                return;
+              reject(error);
             }
-
-            console.log(channelList);
-            return channelList;
+            resolve(channelList);
         });
-    }
+      }
+    });
+    return promise;
+  }
+
+  loadPreviousMessages(groupChannel): Promise<any> {
+    var prevMessageListQuery = groupChannel.createPreviousMessageListQuery();
+    prevMessageListQuery.limit = 30;
+    prevMessageListQuery.reverse = true;
+    const promise = new Promise((resolve, reject) => {
+      prevMessageListQuery.load(function(messages, error) {
+        if (error) {
+          reject(error);
+        }
+        resolve(messages);
+        console.log(messages);
+      });
+    });
+    return promise;
+  }
+
+  onUserReceivedInvitation = (groupChannel, inviter, invitees) => {
+    this.acceptInvitation(groupChannel);
+  }
+
+  acceptInvitation(groupChannel) {
+    // In case of accepting an invitation
+    groupChannel.acceptInvitation(function(response, error) {
+      if (error) {
+          return;
+      }
+    });
+  }
+  
+  declineInvitation() {
+    // In case of declining an invitation
+    this.sendbird.GroupChannel.declineInvitation(function(response, error) {
+      if (error) {
+          return;
+      }
+    });
+  }
+
+  sendMessage(message: string, groupChannel): Promise<any> {
+    const params = new this.sendbird.UserMessageParams();
+
+    params.message = message;
+    params.pushNotificationDeliveryOption = 'default';  // Either 'default' or 'suppress' 
+    const promise = new Promise((resolve, reject) => {
+      groupChannel.sendUserMessage(params, function(message, error) {
+        if (error) {
+            reject(error);
+        }
+        resolve(message);
+        console.log(message);
+      });
+    })
+    return promise;
   }
 
   ngOnDestroy() {
